7548bc8cebacb7dc3094c2cbebe867825db54230
jvrcorona <jvrcorona14@gmail.com>
2021-02-11 16:05:14 -0700

    version2

--- pattern.py
+++ pattern.py
@@ -1,98 +1,93 @@
+#########################
+"""
+Chaos Demo Using Python and simple game
+"""
+#########################
+
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-
-#########################
-"""
-
-
-"""
-#########################
+import math
 
 
 class Pattern:
 
-    #points defined as arrays [x,y]
-    """
-        TRIANGLE POSITIONING
-               p34
-                /\
-            P12/__\P56
-    """
-    p12 = [2,0]
-    p34 = [10,10]
-    p56 = [18,0]
-
-    m1234 = (p12[1]-p34[1]) / (p12[0]-p34[0])
-    m1256 = (p12[1]-p56[1]) / (p12[0]-p56[0])
-    m3456 = (p34[1]-p56[1]) / (p34[0]-p56[0])
-    #if six sided die lands on corresponding number, midpoint to that point
-
     #empy array that will contain all the points to plot
-    points = np.array([]);
+    points = np.array(['no-points']);
+
+
+    def __init__(self, bounding_points):
+        self.n_bounds = len(bounding_points)/2
+        #bounding points bounding_points[:0] -> x-points bounding_points[:,1] -> y-points
+        self.bounding_points = np.reshape(np.array(bounding_points), (-1,2))
+        self.points = np.array([])
 
 
     #returns midpoint of two points
     def findMidpoint(p1, p2):
         #find mids of x and y
+
         xmid = ( p1[0] + p2[0] ) / 2
         ymid = ( p1[1] + p2[1] ) / 2
         return [xmid, ymid]
 
-    def isNotInBounds (p):
-        #yp = mxp - mx1 + y1
-        y1 = Pattern.m1234*p[0] - Pattern.m1234*Pattern.p12[0] + Pattern.p12[1]
-        y2 = Pattern.m1256*p[0] - Pattern.m1256*Pattern.p12[0] + Pattern.p12[1]
-        y3 = Pattern.m3456*p[0] - Pattern.m3456*Pattern.p34[0] + Pattern.p34[1]
-        #print(y1)
-        #print(y2)
-        #print(y3)
-        #P[1] must be >y2 , <y1, <y3
-        if ( p[1] > y2 and p[1] < y1 and p[1] < y3):
-            return False
-        else:
-            return True
-
-    def genRandPoint_Bounded():
-        xmin = min(Pattern.p12[0], Pattern.p34[0], Pattern.p56[0])
-        xmax = max(Pattern.p12[0], Pattern.p34[0], Pattern.p56[0])
-        ymin = min(Pattern.p12[1], Pattern.p34[1], Pattern.p56[1])
-        ymax = max(Pattern.p12[1], Pattern.p34[1], Pattern.p56[1])
+    #loosen bounded requirement since starting point can really be anywhere
+    def genRandPoint_Bounded(self):
+        xmin = min(self.bounding_points[:,0])
+        xmax = max(self.bounding_points[:,0])
+        ymin = min(self.bounding_points[:,1])
+        ymax = max(self.bounding_points[:,1])
         xrand = ( random.random()* (xmax-xmin) ) + xmin
         yrand = ( random.random()* (ymax-ymin) ) + ymin
-        while ( Pattern.isNotInBounds([xrand, yrand]) ):
-            xrand = ( random.random()* (xmax-xmin) ) + xmin
-            yrand = ( random.random()* (ymax-ymin) ) + ymin
         #random.random() returns random float between 0.0 and 1
         return [xrand, yrand]
 
     def rollDie( sides ):
-        return round(random.random() * (sides-1) ) + 1
+        return int(round(random.random() * (sides-1) ) + 1)
 
-    def addPoints( n ):
-        p_start = Pattern.genRandPoint_Bounded()
-        Pattern.points = np.append(Pattern.points, p_start)
+    def addPoints(self, n ):
+        p_start = Pattern.genRandPoint_Bounded(self)
+        self.points = np.append(self.points, p_start)
         p_curr = p_start;
         while ( n > 0 ):
-            dieroll = Pattern.rollDie(6);
-            #if dieroll == 1 0r 2 --> append(findMidpoint(p12, prand))
-            if ( dieroll == 1 or dieroll == 2 ):
-                p_curr = Pattern.findMidpoint(Pattern.p12, p_curr)
-                Pattern.points = np.append(Pattern.points, p_curr )
+            die_sides = int(self.n_bounds)
+            #print('Diesides: ',die_sides)
+            dieroll = Pattern.rollDie(die_sides*2);
+            #print('Dieroll: ', dieroll)
+            #two different cases: die_sides = n_bounds ; die_sides = n_bounds*2
+            if (die_sides == self.n_bounds):
+                #die_sides number of ifs
+                for i in range(die_sides):
+                    #print('in for loop')
+                    if (dieroll == i + 1):
+                        #print('p_curr: ',p_curr,'   point: ', self.bounding_points[i])
+                        p_curr = Pattern.findMidpoint(p_curr, self.bounding_points[i])
+                        self.points = np.append(self.points, p_curr)
+                        #print('next p: ', p_curr)
+                        break
             #if dieroll == 3 0r 4 --> append(findMidpoint(p34, prand))
-            if ( dieroll == 3 or dieroll == 4 ):
-                p_curr = Pattern.findMidpoint(Pattern.p34, p_curr)
-                Pattern.points = np.append(Pattern.points, p_curr )
-            #if dieroll == 5 0r 6 --> append(findMidpoint(p56, prand))
-            if ( dieroll == 5 or dieroll == 6 ):
-                p_curr = Pattern.findMidpoint(Pattern.p56, p_curr)
-                Pattern.points = np.append(Pattern.points, p_curr )
+            if (die_sides == 2*self.n_bounds):
+                #die_sides number of ifs
+                for i in range(die_sides/2):
+                    #print('in for loop')
+                    if (int(math.floor(dieroll/2)) == i ):
+                        #print('p_curr: ',p_curr,'   point: ', self.bounding_points[i])
+                        p_curr = Pattern.findMidpoint(p_curr, self.bounding_points[i])
+                        self.points = np.append(self.points, p_curr)
+                        #print('next p: ', p_curr)
+                        break
             n = n-1
-
-
-Pattern.addPoints(1000)
-points = np.reshape(Pattern.points, (-1,2))
-x = points[:,0]
-y = points[:,1]
-plt.scatter(x,y)
-plt.show()
+    #plot the points_new
+    def plotPoints(self, n):
+        """
+            things to add:
+                *add animation
+                *change styling
+                    -different size dots
+                    -different color for bounding points
+        """
+        self.addPoints(n)
+        points_new = np.reshape(self.points,(-1,2))
+        plt.scatter(self.bounding_points[:,0], self.bounding_points[:,1], s=20, c='red')
+        plt.scatter(points_new[:,0], points_new[:,1], s=1, c='blue')
+        plt.show()
